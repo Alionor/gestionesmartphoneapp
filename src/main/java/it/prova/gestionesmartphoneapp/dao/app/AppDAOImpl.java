470ede41f8c027686252bb843c48476cce002b16
Alionor <elegrisa32@libero.it>
2026-03-11 17:30:20 +0100

    Metodi crud nel DAO implmentati

---
+++ src/main/java/it/prova/gestionesmartphoneapp/dao/app/AppDAOImpl.java
@@ -0,0 +1,47 @@
+package it.prova.gestionesmartphoneapp.dao.app;
+
+import it.prova.gestionesmartphoneapp.model.App;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class AppDAOImpl implements AppDAO {
+
+    private EntityManager entityManager;
+
+    @Override
+    public List<App> findAll() throws Exception {
+        return entityManager.createQuery("from App", App.class).getResultList();
+    }
+
+    @Override
+    public App findById(Long id) throws Exception {
+        if (id < 0) throw new Exception("Id non valido.");
+        return entityManager.find(App.class, id);
+    }
+
+    @Override
+    public void update(App app) throws Exception {
+        if (app == null) throw new Exception("Oggetto inserito non valido.");
+        entityManager.merge(app);
+    }
+
+    @Override
+    public void insert(App app) throws Exception {
+        if (app == null) throw new Exception("Oggetto inserito non valido.");
+        entityManager.persist(app);
+    }
+
+    @Override
+    public void delete(Long id) throws Exception {
+        if (id < 0) throw new Exception("Id non valido.");
+        entityManager.createQuery("DELETE FROM App where id = ?1")
+                .setParameter(1, id).executeUpdate();
+    }
+
+    @Override
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+}
---
+++ src/main/java/it/prova/gestionesmartphoneapp/dao/smartphone/SmartphoneDAOImpl.java
@@ -0,0 +1,47 @@
+package it.prova.gestionesmartphoneapp.dao.smartphone;
+
+import it.prova.gestionesmartphoneapp.model.Smartphone;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class SmartphoneDAOImpl implements SmartphoneDAO {
+
+    private EntityManager entityManager;
+
+    @Override
+    public List<Smartphone> findAll() throws Exception {
+        return entityManager.createQuery("from Smartphone", Smartphone.class).getResultList();
+    }
+
+    @Override
+    public Smartphone findById(Long id) throws Exception {
+        if (id < 0) throw new Exception("Id non valido.");
+        return entityManager.find(Smartphone.class, id);
+    }
+
+    @Override
+    public void update(Smartphone smartphone) throws Exception {
+        if (smartphone == null) throw new Exception("Oggetto inserito non valido.");
+        entityManager.merge(smartphone);
+    }
+
+    @Override
+    public void insert(Smartphone smartphone) throws Exception {
+        if (smartphone == null) throw new Exception("Oggetto inserito non valido.");
+        entityManager.persist(smartphone);
+    }
+
+    @Override
+    public void delete(Long id) throws Exception {
+        if (id < 0) throw new Exception("Id non valido.");
+        entityManager.createQuery("DELETE FROM Smartphone where id = ?1")
+                .setParameter(1, id).executeUpdate();
+    }
+
+    @Override
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+}
